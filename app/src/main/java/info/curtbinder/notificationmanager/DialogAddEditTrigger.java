3d0bddabdf06c519a190105820b43e462aca47f8
Curt Binder <curt.binder@gmail.com>
2014-09-20 23:09:03 -0500

    Fixed add/edit notification dialog
    changed to tablelayout from relativelayout to better handle displaying the information in a more structured layout

--- app/src/main/java/info/curtbinder/notificationmanager/DialogAddEditTrigger.java
+++ app/src/main/java/info/curtbinder/notificationmanager/DialogAddEditTrigger.java
@@ -10,9 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
